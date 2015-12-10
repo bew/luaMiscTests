bb3e908ab4cd44e4142bec750531fbb5fe6d688a
lesell_b <lesell_b@epitech.eu>
2015-12-10 17:32:26 +0100

    add basic test with LuaSocket : server + multiple clients

---
+++ socket/basic/client.lua
@@ -0,0 +1,39 @@
+local Socket = require"socket"
+local MsgPack = require"MessagePack"
+
+
+
+host = host or "localhost"
+port = port or 1234
+if arg then
+	host = arg[1] or host
+	port = arg[2] or port
+end
+
+local tcp = assert(Socket.tcp())
+local success, status = tcp:connect(host, port)
+if status == "connection refused" then
+	print()
+	print("Cannot connect to server on " .. host .. ":" .. port)
+	print("You must first start the server.lua")
+	print()
+	os.exit(false)
+end
+
+while true do
+	local msg, status, err = tcp:receive();
+
+	if status == "closed" then
+		break
+	end
+
+	if err then
+		print(err)
+	else
+
+		print(msg)
+
+	end
+end
+
+print("finish")
---
+++ socket/basic/server.lua
@@ -0,0 +1,63 @@
+-----------------------------------------------------------------------------
+-- TCP sample: Little program to send text lines to a given host/port
+-- LuaSocket sample files
+-- Author: Diego Nehab
+-----------------------------------------------------------------------------
+
+local Socket = require("socket")
+local MsgPack = require"MessagePack"
+
+host = host or "localhost"
+port = port or 1234
+if arg then
+	host = arg[1] or host
+	port = arg[2] or port
+end
+
+print("Attempting connection to host '" ..host.. "' and port " ..port.. "...")
+
+local tcp = assert(Socket.tcp())
+assert(tcp:bind(host, port))
+assert(tcp:listen(10))
+
+print("Server is ready, client can connect to " .. host .. ":" .. port)
+
+local connectedClients = {}
+
+local counter = 0
+
+while true do
+
+	local readSocks = Socket.select({tcp}, nil, 0)
+	if readSocks and readSocks[1] == tcp then
+		tcp:settimeout(0) -- non blocking accept
+		local newClient = tcp:accept()
+		if newClient then
+			table.insert(connectedClients, newClient)
+			print("We have a new client !!", newClient)
+		end
+	end
+
+	local deadClients = {}
+	for i, client in ipairs(connectedClients) do
+		local _, status, err = client:send("Number is " .. counter .. "\n")
+
+		if status == "closed" then
+			print("A client disconnected..", client)
+			table.insert(deadClients, i)
+		end
+	end
+
+	for _, id in ipairs(deadClients) do
+		table.remove(connectedClients, id)
+	end
+	deadClients = nil
+
+	Socket.sleep(1) -- wait 1 sec
+
+	counter = counter + 1
+
+end
+
+print("finish")
+print("bye")
